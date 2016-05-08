8083cc473f54f5cf479ed4e68d076e8731855153
Dan Jacob <danjac354@gmail.com>
2016-05-08 12:15:18 +0300

    Basic layout

--- ui/src/components/app.js
+++ ui/src/components/app.js
@@ -1,24 +1,94 @@
 import React from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import * as bs from 'react-bootstrap';
 
 require('bootstrap/dist/css/bootstrap.min.css');
 require('bootstrap/dist/css/bootstrap-theme.min.css');
 
-const starmap = require('./starmap.png');
+const starmap = require('../starmap.png');
+
+const worlds = [
+  {
+    name: 'Meresankh',
+    coords: '0101',
+    uwp: 'C849BBA-C',
+    bases: '',
+    notes: 'Hi In',
+  },
+  {
+    name: 'Ushkhakegasas',
+    coords: '0107',
+    uwp: 'CA365BBA-D',
+    bases: 'N T I',
+    notes: 'Hi',
+  },
+  {
+    name: 'Aszagin',
+    coords: '0401',
+    uwp: 'E242358-7',
+    bases: '',
+    notes: 'Lo Na Po',
+  },
+];
+
 
 class App extends React.Component {
   render() {
     return (
-      <Grid>
-        <Row>
-          <Col md={6}>
-            Stats go here....
-          </Col>
-          <Col md={6}>
+      <bs.Grid>
+        <h2>Subsector Merengha</h2>
+        <bs.Row>
+          <bs.Col md={8}>
+            <bs.ButtonGroup>
+              <bs.Button><bs.Glyphicon glyph="star" /> New subsector</bs.Button>
+              <bs.Button><bs.Glyphicon glyph="download" /> Download CSV</bs.Button>
+              <bs.Button><bs.Glyphicon glyph="download" /> Download map (PNG)</bs.Button>
+            </bs.ButtonGroup>
+          </bs.Col>
+          <bs.Col md={4}>
+            <bs.FormGroup>
+              <bs.InputGroup>
+                <bs.InputGroup.Addon>
+                  <bs.Glyphicon glyph="search" />
+                </bs.InputGroup.Addon>
+                <bs.FormControl
+                  type="search"
+                  placeholder="Find world or subsector..."
+                />
+              </bs.InputGroup>
+              <bs.FormControl.Feedback />
+            </bs.FormGroup>
+          </bs.Col>
+        </bs.Row>
+        <bs.Row>
+          <bs.Col md={8}>
+            <table className="table table-striped table-condensed table-hover">
+              <thead>
+                <tr>
+                  <th>Coords</th>
+                  <th>Name</th>
+                  <th>UWP</th>
+                  <th>Bases</th>
+                  <th>Notes</th>
+                </tr>
+              </thead>
+              <tbody>
+              {worlds.map((w) => (
+                <tr key={w.coords}>
+                  <td>{w.coords}</td>
+                  <td>{w.name}</td>
+                  <td>{w.uwp}</td>
+                  <td>{w.bases}</td>
+                  <td>{w.notes}</td>
+                </tr>
+                ))}
+              </tbody>
+            </table>
+          </bs.Col>
+          <bs.Col md={4}>
             <img src={starmap} />
-          </Col>
-        </Row>
-      </Grid>
+          </bs.Col>
+        </bs.Row>
+      </bs.Grid>
     );
   }
 }
