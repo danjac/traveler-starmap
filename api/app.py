bb03f6dcd5bc803736fc7f955bfa7046139d9451
Dan Jacob <danjac354@gmail.com>
2016-05-26 00:08:58 +0300

    Fix name path

--- api/app.py
+++ api/app.py
@@ -219,7 +219,7 @@ def create_subsector():
         Path(__file__).parent / 'names.txt',
     )
 
-    subsector = generator.generate_subsector(names_file)
+    subsector = generator.generate_subsector("./names.txt")
 
     db.session.add(subsector)
     db.session.commit()
