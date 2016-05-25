68a2cc99a5861c848625f6016da3f060dc7c0eff
Dan Jacob <danjac354@gmail.com>
2016-05-25 23:26:50 +0300

    Bind to correct port

--- api/config.py
+++ api/config.py
@@ -15,10 +15,10 @@ class Development(Config):
 
 class _Production(Config):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SERVER_NAME = '0.0.0.0:80'
 
     def __init__(self):
-        self.SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+        self.SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+        self.SERVER_NAME = '0.0.0.0:{}'.format(os.environ['PORT'])
 
 
 Production = _Production()
