7e90cc217b14515d1a1b2e1f0900c7f05868001e
Dan Jacob <danjac354@gmail.com>
2016-05-25 23:45:06 +0300

    Bind to correct port

--- api/app.py
+++ api/app.py
@@ -239,4 +239,8 @@ if __name__ == "__main__":
 
     with app.app_context():
         db.create_all()
-    app.run(host=app.config['HOST'], port=app.config['PORT'])
+    print(app.config)
+    app.run(
+        host=app.config['HOST'],
+        port=app.config['PORT']
+    )
